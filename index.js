8191292ff347db7231771e50205356127d725c2a
Eirik Backer <Eirik.Backer@nrk.no>
2019-01-14 13:41:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+import postcss from 'postcss'
+import fs from 'fs'
+
+export default postcss.plugin('classNamesToMixins', (opts = {}) => {
+  return (root, result) => {
+    const cssArray = postcssToArray(root)
+
+    Object.keys(opts).forEach((ext) =>
+      fs.writeFileSync(opts[ext], toString(nestRules(cssArray, ext)))
+    )
+  }
+})
+
+function postcssToArray (root) {
+  const rules = []
+
+  root.nodes.forEach((node) => {
+    if (node.type === 'atrule') rules.push([`@${node.name} ${node.params}`, postcssToArray(node)])
+    if (node.type === 'rule') {
+      const style = node.nodes.reduce((acc, { prop, value, important }) => {
+        return acc.concat(prop ? [[prop, `${value}${important ? '!important' : ''}`]] : [])
+      }, [])
+      node.selector.split(',').forEach((selector) => {
+        rules.push([selector.trim(), style])
+      })
+    }
+  })
+
+  return rules
+}
+
+function nestRules (rules, target, nested = [], mediaQuery = false) {
+  rules.forEach(([selector, rule]) => {
+    const isAtRule = selector.match(/@(media|supports)/)
+    const className = selector.match(/\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*/)
+
+    if (isAtRule) nestRules(rule, target, nested, selector)
+    else if (className) {
+      let ruleset = rule
+      let mixin = className[0].slice(1)
+      switch (target) {
+        case 'styl': mixin = `${mixin}()`; break
+        case 'less': mixin = `.${mixin}()`; break
+        case 'scss': mixin = `@mixin ${mixin}`; break
+      }
+      const parent = nested.filter(([m]) => m === mixin)[0] || nested[nested.push([mixin, []]) - 1]
+      const prefix = selector.slice(0, className.index)
+      const suffix = selector.slice(className.index + className[0].length).replace(className[0], '&')
+
+      if (prefix) {
+        if (target !== 'scss') ruleset = [[`${prefix}&${suffix}`, rule]]
+        else ruleset = [[`@at-root ${prefix}#{&}${suffix.replace('&', '#{&}')}`, rule]]
+      } else if (suffix) {
+        ruleset = [[`&${suffix}`, rule]]
+      }
+
+      if (mediaQuery) ruleset = [[mediaQuery, ruleset]]
+
+      parent[1].push(...ruleset)
+    } else {
+      nested.push([selector, rule])
+    }
+  })
+
+  return nested
+}
+
+function toString (rules, pad = '') {
+  return rules.map(([key, val]) => {
+    if (typeof val === 'string') return `${pad}${key}: ${val};\n`
+    return `${pad}${key} {\n${toString(val, pad + '  ')}${pad}}\n`
+  }).join('')
+}
+
+// If ever needed to use without postcss, this also works:
+// const { JSDOM } = require('jsdom')
+// const dom = new JSDOM(`<style>${fs.readFileSync('./lib/core-css.css')}</style>`)
+// const css = dom.window.document.styleSheets[0]
+// // Returns array of [[selector, props], ...] to support multiple equal selectors and consistent order
+// function cssToArray (root) {
+//   const rules = []
+//
+//   Array.from(root.cssRules).forEach((rule) => {
+//     const type = rule.constructor.name.replace(/^CSS|Rule$/g, '').toLowerCase()
+//
+//     if (type === 'media') rules.push([`@media ${Array.from(rule.media).join(',')}`, cssToArray(rule)])
+//     else if (type === 'supports') rules.push([`@supports ${rule.conditionText}`, cssToArray(rule)])
+//     else if (type === 'keyframes') rules.push([`@keyframes ${rule.name}`, cssToArray(rule)])
+//     else if (type === 'style' || type === 'keyframe') {
+//       const selector = rule.selectorText || rule.keyText
+//       const style = Array.from(rule.style).map((prop) => {
+//         const priority = rule.style.getPropertyPriority(prop).replace(/./, '!$&')
+//         return [prop, `${rule.style.getPropertyValue(prop)}${priority}`]
+//       })
+//       selector.split(',').forEach((selector) => {
+//         rules.push([selector.trim(), style])
+//       })
+//     }
+//   })
+//
+//   return rules
+// }
