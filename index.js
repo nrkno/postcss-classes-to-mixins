66a33c8f12a5e9cff1d8c578e39633198d6bb27d
Torstein Bjørnstad <torstein.bjornstad@nrk.no>
2024-11-04 13:42:20 +0100

    Nest scss rules following other nested rules
    In order to avoid deprecation warning described here: https://sass-lang.com/documentation/breaking-changes/mixed-decls/

--- index.js
+++ index.js
@@ -72,6 +72,14 @@ function nestRules ({ rules, target, nested = [], mediaQuery = false, mixinsOnly
         else ruleset = [[`@at-root ${prefix}#{&}${suffix.replace('&', '#{&}')}`, rule]]
       } else if (suffix) {
         ruleset = [[`&${suffix}`, rule]]
+      }else {
+        // https://sass-lang.com/documentation/breaking-changes/mixed-decls/
+        if(target === 'scss') {
+          const isNestedSelector = parent[1].some(([selector]) => selector.startsWith('&'));
+          if(isNestedSelector) {
+            ruleset = [['&', rule]]
+          }
+        }
       }
 
       // Include keyframes
