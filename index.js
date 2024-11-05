35e10b2fb7d8ddd536a56c6c026a475f63a2e11d
Torstein Bjørnstad <torstein.bjornstad@nrk.no>
2024-11-05 12:05:11 +0100

    Add some checks around parent ruleset access

--- index.js
+++ index.js
@@ -73,10 +73,12 @@ function nestRules ({ rules, target, nested = [], mediaQuery = false, mixinsOnly
       } else if (suffix) {
         ruleset = [[`&${suffix}`, rule]]
       } else {
-        // https://sass-lang.com/documentation/breaking-changes/mixed-decls/
         if (target === 'scss') {
-          const isNestedSelector = parent[1].some(([selector]) => selector.startsWith('&'))
-          if (isNestedSelector) {
+          // https://sass-lang.com/documentation/breaking-changes/mixed-decls/
+          // If appending plain rule following any nested rules, it must be wrapped in a & {} block
+          const parentRuleset = parent?.[1];
+          const hasNestedSelector = Array.isArray(parentRuleset) && parentRuleset.some(([selector]) => selector.startsWith('&'))
+          if (hasNestedSelector) {
             ruleset = [['&', rule]]
           }
         }
